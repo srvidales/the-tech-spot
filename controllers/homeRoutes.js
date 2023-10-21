a3ce31ff720d8ee4dd24aac24a3ff62534d22660
Sergio Vidales Perez <srvidales@gmail.com>
2023-10-21 02:25:51 -0700

    Add route authentication and client js authentication scripts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -30,7 +30,7 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
@@ -47,8 +47,23 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-router.get('/dashboard', async (req, res) => {
-  res.render('dashboard');
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
