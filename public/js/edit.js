4f55b4daff3c2a361c497a5b26e6dfe1d255173f
Sergio Vidales Perez <srvidales@gmail.com>
2023-11-29 04:31:38 -0800

    Implement delete post and fixes

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,13 +1,13 @@
-const newFormHandler = async (event) => {
+const updateFormHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector('#post-title').value.trim();
-  const content = document.querySelector('#post-content').value.trim();
+  const description = document.querySelector('#post-description').value.trim();
 
-  if (title && content) {
+  if (title && description) {
     const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ title, content }),
+      body: JSON.stringify({ title, description }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -21,10 +21,28 @@ const newFormHandler = async (event) => {
   }
 };
 
+const delButtonHandler = async (event) => {
+  event.preventDefault();
+
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
 document
   .querySelector('.update-post-form')
-  .addEventListener('submit', newFormHandler);
+  .addEventListener('submit', updateFormHandler);
 
 document
   .querySelector('#button-delete')
-  .addEventListener('submit', deletePostHandler);
+  .addEventListener('click', delButtonHandler);
